ab24e4eb3fd4e7f9b5ee0a6074e1e10af095dc0f
Widren <kadirsen552@gmail.com>
2022-06-12 18:07:43 +0300

    upgrade

---
+++ eShopOnContainers/eShopOnContainers.UnitTests/Services/BasketServiceTests.cs
@@ -0,0 +1,16 @@
+﻿using System.Threading.Tasks;
+using Xunit;
+
+namespace eShopOnContainers.UnitTests
+{
+    public class BasketServiceTests
+    {
+        [Fact]
+        public async Task GetFakeBasketTest()
+        {
+            //var catalogMockService = new CatalogMockService();       
+            //var result  = await catalogMockService.GetCatalogAsync();
+            //Assert.NotEmpty(result);
+        }
+    }
+}
---
+++ eShopOnContainers/eShopOnContainers.UnitTests/Services/OrdersServiceTests.cs
@@ -0,0 +1,27 @@
+﻿using eShopOnContainers.Core;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace eShopOnContainers.UnitTests
+{
+    public class OrdersServiceTests
+    {
+		[Fact]
+		public async Task GetFakeOrderTest()
+		{
+			//var ordersMockService = new OrderMockService();
+			//var order = await ordersMockService.GetOrderAsync(1, GlobalSetting.Instance.AuthToken);
+
+			//Assert.NotNull(order);
+		}
+		
+        [Fact]
+        public async Task GetFakeOrdersTest()
+        {
+            //var ordersMockService = new OrderMockService();
+            //var result = await ordersMockService.GetOrdersAsync(GlobalSetting.Instance.AuthToken);
+
+            //Assert.NotEmpty(result);
+        }
+    }
+}
